ee3f2eccc725a4a99a36e8f6ca5ea9074528e1aa
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-15 21:11:30 +0500

    done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,26 @@
+import { prata } from '@/lib/fonts/font'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Italianno } from 'next/font/google'
+
+const italianno = Italianno({
+  weight: "400"
+})
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='max-w-7xl flex flex-col items-center justify-center relative mx-auto border-t border-main'>
+       <Image src={"/spin.svg"} alt='spinning image' width={150} height={150} className='spin-slow absolute left-45 top-5 z-10' />
+      <Link href="/" className="flex flex-col items-center md:items-start">
+        <div className='pt-20'>
+          <p className={`${prata.className} font-semibold text-xl md:text-[106px] leading-10 uppercase text-center`}>Ayyan</p>
+          <p className={`${italianno.className} text-[25px] md:text-[140px]`}>
+            Event And Catering
+          </p>
+          </div>
+        </Link>
+    </footer>
   )
 }
 
