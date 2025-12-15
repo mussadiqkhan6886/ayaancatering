7403efa8b5bec8e5838396a9476bd6237d263878
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-15 15:53:01 +0500

    done home now mkaing both resp

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,9 +1,55 @@
+import { prata } from '@/lib/fonts/font'
 import React from 'react'
+import {
+  FiInstagram,
+  FiFacebook,
+  FiArrowDown,
+  FiArrowDownCircle
+} from "react-icons/fi"
+
+import {FaPhone, FaWhatsapp} from "react-icons/fa"
+
 
 const Hero = () => {
   return (
-    <section>
-      <video src="hero.mp4" muted autoPlay loop></video>
+    <section className="relative w-full h-screen overflow-hidden">
+      
+      {/* Background Video */}
+      <video
+        src="/hero.mp4"
+        muted
+        autoPlay
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+
+      {/* Overlay (optional for readability) */}
+      <div className="absolute inset-0 bg-black/40"></div>
+
+      {/* Center Heading */}
+      <h1
+        className={`absolute left-1/2 top-1/2 
+        -translate-x-1/2 -translate-y-1/2
+        text-[50px] font-bold text-white uppercase text-center
+        ${prata.className}`}
+      >
+        Create a memory that lasts a lifetime
+      </h1>
+
+      <div className='absolute left-1/2  text-white bottom-25 -translate-x-1/2 flex items-center justify-center flex-col'>
+          <p className='text-2xl mb-5'>See Our Work</p>
+          <FiArrowDown className='text-[40px] animate-ping' />
+      </div>
+
+      {/* Social Icons */}
+      <div className="absolute bottom-18 right-7 flex flex-col gap-4 text-white text-xl">
+        <FaPhone className='rounded-full border border-white w-10 h-10 p-2' />
+        <FaWhatsapp className='rounded-full border border-white w-10 h-10 p-2' />
+        <FiInstagram className='rounded-full border border-white w-10 h-10 p-2' />
+        <FiFacebook className='rounded-full border border-white w-10 h-10 p-2' />
+      </div>
+
     </section>
   )
 }
